019ba88ca364e0e9b0ad81d8f38cdf55fbbfbeed
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-05-01 08:58:05 -0600

    Change to detecting t-rex center ILO title center.

--- main.py
+++ main.py
@@ -13,31 +13,31 @@ driver = webdriver.Chrome(executable_path=CHROMEDRIVER)
 driver.get(WEBPAGE)
 current_time = time.time()
 loop_stop = current_time + 60
+DETECT_DISTANCE = 75
 
 
 def run_game():
     try:
-        title_center_x, title_center_y = pyautogui.locateCenterOnScreen(
-            "./images/title.png"
-        )
+        trex_x, trex_y = pyautogui.locateCenterOnScreen("./images/t-rex.png")
     except TypeError:
         pyautogui.alert(
             text="Game Error - Restart App", title="GAME ERROR", button="OK"
         )
         driver.quit()
-    obstacle_x, obstacle_y = title_center_x - 185, title_center_y + 270
-    gameover_x, gameover_y = title_center_x, title_center_y + 227
+    obstacle_x, obstacle_y = trex_x + DETECT_DISTANCE, trex_y + 15
+    gameover_x, gameover_y = trex_x + 255, trex_y - 5
     pyautogui.alert(
         text="Ready to autoplay for 60 seconds.", title="Game Start", button="OK"
     )
     pyautogui.press("up")
     while time.time() < loop_stop:
         if pyautogui.pixelMatchesColor(
-            obstacle_x, obstacle_y, (83, 83, 83), tolerance=10
+            obstacle_x, obstacle_y, (83, 83, 83), tolerance=5
         ):  # Jump for obstacle
             pyautogui.press("up")
+            time.sleep(0.2)  # Jumping over cacti
         elif pyautogui.pixelMatchesColor(
-            gameover_x, gameover_y, (83, 83, 83), tolerance=10
+            gameover_x, gameover_y, (83, 83, 83), tolerance=5
         ):  # Reset game
             pyautogui.press("up")
     pyautogui.alert(text="I'm Done!!", title="Game Over", button="OK")
@@ -46,9 +46,3 @@ def run_game():
 
 if __name__ == "__main__":
     run_game()
-
-# Detect location of dino and save as variable
-
-# Detect incoming cacti and track
-
-# Determine jump timing and react to moving cacti

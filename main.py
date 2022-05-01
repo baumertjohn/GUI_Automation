ccdc103242379c33e130c55c0120bf5eddad1bcb
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-05-01 08:13:10 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# 100 Days of Code
+# GUI Automation - Autoplay Google's T-Rex Game
+
+import time
+
+import pyautogui
+from selenium import webdriver
+
+WEBPAGE = "https://elgoog.im/t-rex/"
+CHROMEDRIVER = "D:/Python_Code/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER)
+
+driver.get(WEBPAGE)
+current_time = time.time()
+loop_stop = current_time + 60
+
+
+def run_game():
+    try:
+        title_center_x, title_center_y = pyautogui.locateCenterOnScreen(
+            "./images/title.png"
+        )
+    except TypeError:
+        pyautogui.alert(
+            text="Game Error - Restart App", title="GAME ERROR", button="OK"
+        )
+        driver.quit()
+    obstacle_x, obstacle_y = title_center_x - 185, title_center_y + 270
+    gameover_x, gameover_y = title_center_x, title_center_y + 227
+    pyautogui.alert(
+        text="Ready to autoplay for 60 seconds.", title="Game Start", button="OK"
+    )
+    pyautogui.press("up")
+    while time.time() < loop_stop:
+        if pyautogui.pixelMatchesColor(
+            obstacle_x, obstacle_y, (83, 83, 83), tolerance=10
+        ):  # Jump for obstacle
+            pyautogui.press("up")
+        elif pyautogui.pixelMatchesColor(
+            gameover_x, gameover_y, (83, 83, 83), tolerance=10
+        ):  # Reset game
+            pyautogui.press("up")
+    pyautogui.alert(text="I'm Done!!", title="Game Over", button="OK")
+    driver.quit()
+
+
+if __name__ == "__main__":
+    run_game()
+
+# Detect location of dino and save as variable
+
+# Detect incoming cacti and track
+
+# Determine jump timing and react to moving cacti
